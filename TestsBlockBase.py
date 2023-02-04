57c75c2c33e77d33eee25917c1fa27dd550f74c6
Sashs <sashasam348@gmail.com>
2023-02-04 12:59:55 +0200

    Finished corners test

--- TestsBlockBase.py
+++ TestsBlockBase.py
@@ -120,6 +120,48 @@ class TestBlock(unittest.TestCase):
         self.assertEqual(self.ball.direction_y, -1)
         self.assertEqual(self.ball.direction_x, -1)
 
+    def test_corner_top_right_bounce(self):
+        #                         /
+        #                        ^
+        # this kind of bounce   /
+        #                 _____v
+        #                      |
+        self.block.top = 500
+        self.block.right = 500
+        self.ball.direction_x = -1
+        self.ball.direction_y = 1
+        self.block.process_collision(self.ball)
+        self.assertEqual(self.ball.direction_y, -1)
+        self.assertEqual(self.ball.direction_x, 1)
+
+    def test_corner_bottom_right_bounce(self):
+        #                 _____|
+        #                      ^
+        # this kind of bounce   \
+        #                        v
+        #                         \
+        self.block.bottom = 500
+        self.block.right = 500
+        self.ball.direction_x = -1
+        self.ball.direction_y = -1
+        self.block.process_collision(self.ball)
+        self.assertEqual(self.ball.direction_y, 1)
+        self.assertEqual(self.ball.direction_x, 1)
+
+    def test_corner_bottom_left_bounce(self):
+        #                       |_____|
+        #                      ^
+        # this kind of bounce /
+        #                    v
+        #                   /
+        self.block.bottom = 500
+        self.block.left = 500
+        self.ball.direction_x = 1
+        self.ball.direction_y = -1
+        self.block.process_collision(self.ball)
+        self.assertEqual(self.ball.direction_y, 1)
+        self.assertEqual(self.ball.direction_x, -1)
+
     pass
 
 
