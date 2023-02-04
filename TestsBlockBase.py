6d1094c214d9d5dae30e16c6a25841372d7558ec
Sashs <sashasam348@gmail.com>
2023-02-04 12:57:49 +0200

    Added straightforward tests

--- TestsBlockBase.py
+++ TestsBlockBase.py
@@ -11,6 +11,65 @@ class TestBlock(unittest.TestCase):
         self.block = Paddle(200, 200, 15, 1000, 1000)
         self.block.top = 500
 
+    def test_top_bounce(self):
+        self.ball.direction_x = 1
+        self.ball.direction_y = 1
+        self.block.process_collision(self.ball)
+        self.assertEqual(self.ball.direction_y, -1)
+        self.assertEqual(self.ball.direction_x, 1)
+
+        self.ball.direction_x = -1
+        self.ball.direction_y = 1
+        self.block.process_collision(self.ball)
+        self.assertEqual(self.ball.direction_y, -1)
+        self.assertEqual(self.ball.direction_x, -1)
+
+    def test_bottom_bounce(self):
+        self.block.bottom = 500
+        self.ball.direction_x = 1
+        self.ball.direction_y = -1
+        self.block.process_collision(self.ball)
+        self.assertEqual(self.ball.direction_y, 1)
+        self.assertEqual(self.ball.direction_x, 1)
+
+        self.block.bottom = 500
+        self.ball.direction_x = -1
+        self.ball.direction_y = -1
+        self.block.process_collision(self.ball)
+        self.assertEqual(self.ball.direction_y, 1)
+        self.assertEqual(self.ball.direction_x, -1)
+    pass
+
+    def test_right_bounce(self):
+        self.block.top = 400
+        self.block.left = 505
+        self.ball.direction_x = 1
+        self.ball.direction_y = -1
+        self.block.process_collision(self.ball)
+        self.assertEqual(self.ball.direction_y, -1)
+        self.assertEqual(self.ball.direction_x, -1)
+
+        self.ball.direction_x = 1
+        self.ball.direction_y = 1
+        self.block.process_collision(self.ball)
+        self.assertEqual(self.ball.direction_y, 1)
+        self.assertEqual(self.ball.direction_x, -1)
+
+    def test_left_bounce(self):
+
+        self.block.top = 400
+        self.block.right = 505
+        self.ball.direction_x = -1
+        self.ball.direction_y = -1
+        self.block.process_collision(self.ball)
+        self.assertEqual(self.ball.direction_y, -1)
+        self.assertEqual(self.ball.direction_x, 1)
+
+        self.ball.direction_x = -1
+        self.ball.direction_y = 1
+        self.block.process_collision(self.ball)
+        self.assertEqual(self.ball.direction_y, 1)
+        self.assertEqual(self.ball.direction_x, 1)
 
     pass
 
