087c169d06755d98ed27a2c843d852836f7110ef
Sashs <sashasam348@gmail.com>
2023-02-04 12:55:42 +0200

    Added tests file

---
+++ TestsBlockBase.py
@@ -0,0 +1,14 @@
+import unittest
+from Ball import Ball
+from Pallette import Paddle
+
+
+class TestBlock(unittest.TestCase):
+    def setUp(self):
+
+
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
