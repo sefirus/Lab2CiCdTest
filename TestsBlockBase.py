35c725cdb35c96d157838f1d598fd7723e15b6cd
Sashs <sashasam348@gmail.com>
2023-02-04 12:56:34 +0200

    Implemented setup

--- TestsBlockBase.py
+++ TestsBlockBase.py
@@ -1,10 +1,15 @@
 import unittest
 from Ball import Ball
-from Pallette import Paddle
+from Paddle import Paddle
 
 
 class TestBlock(unittest.TestCase):
     def setUp(self):
+        self.ball = Ball(10, 6, 1000, 1000)
+        self.ball.x = 495
+        self.ball.y = 495
+        self.block = Paddle(200, 200, 15, 1000, 1000)
+        self.block.top = 500
 
 
     pass
