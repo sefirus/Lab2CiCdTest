af19199a8d779474afca016d525999b2f41960b4
Alina Darmoroz <daarmorozaa@gmail.com>
2023-02-08 20:16:08 +0200

    Moved game setup logic to the constructor

--- Game.py
+++ Game.py
@@ -4,29 +4,13 @@ from Ball import Ball
 from Block import Block
 from Paddle import Paddle
 
+
 class Game:
     SCREEN_WIDTH, SCREEN_HEIGHT = 1200, 800
-    FPS = 120
-
-    def normalize_input(self):
-        if int(self.args.difficulty) > 3 or int(self.args.difficulty) < 0:
-            self.args.difficulty_delta = 1
-
-    def adjust_difficulty(self):
-        self.normalize_input()
-        if int(self.args.difficulty) == 1:
-            self.args.paddle_width = 330
-            self.diff = 0.2
-        elif int(self.args.difficulty) == 2:
-            self.args.paddle_width = 270
-            self.diff = 0.5
-        else:
-            self.args.paddle_width = 240
-            self.diff = 0.7
+    FPS = 60
 
     def __init__(self, args):
         self.args = args
-        self.adjust_difficulty()
         # paddle settings
         paddle_w = int(self.args.paddle_width)
         paddle_h = int(self.args.paddle_height)
@@ -34,16 +18,16 @@ class Game:
         self.paddle = Paddle(paddle_w, paddle_h, self.paddle_speed, Game.SCREEN_HEIGHT, Game.SCREEN_WIDTH)
         # ball settings
         ball_radius = int(self.args.ball_radius)
-        ball_speed = int(self.args.ball_speed)
-        self.ball = Ball(ball_radius, ball_speed, Game.SCREEN_HEIGHT, Game.SCREEN_WIDTH)
+        self.ball_speed = int(self.args.ball_speed)
+        self.diff = float(self.args.difficulty_delta)
+        self.ball = Ball(ball_radius, self.ball_speed, Game.SCREEN_HEIGHT, Game.SCREEN_WIDTH)
         # blocks settings
         self.block_list = [Block(i, j) for i in range(7) for j in range(4)]
 
     def process_bounds_collisions(self):
-        if self.ball.centerx < self.ball.radius - self.ball.speed \
-                or self.ball.centerx > Game.SCREEN_WIDTH - self.ball.radius + self.ball.speed:
+        if self.ball.centerx < self.ball.radius or self.ball.centerx > Game.SCREEN_WIDTH - self.ball.radius:
             self.ball.direction_x = -self.ball.direction_x
-        if self.ball.centery < self.ball.radius - self.ball.speed:
+        if self.ball.centery < self.ball.radius:
             self.ball.direction_y = -self.ball.direction_y
 
     def process_blocks_collisions(self):
@@ -72,8 +56,8 @@ class Game:
             pygame.draw.rect(sc, pygame.Color('orange'), self.paddle)
             pygame.draw.circle(sc, pygame.Color('white'), self.ball.center, self.ball.radius)
             # ball movement
-            self.ball.x += self.ball.speed * self.ball.direction_x
-            self.ball.y += self.ball.speed * self.ball.direction_y
+            self.ball.x += self.ball_speed * self.ball.direction_x
+            self.ball.y += self.ball_speed * self.ball.direction_y
             # collision with bounds
             self.process_bounds_collisions()
             # collision with blocks
