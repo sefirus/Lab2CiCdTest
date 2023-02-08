1ba7b2af9287b129c39d95b0173dd27d34bce739
Alina Darmoroz <daarmorozaa@gmail.com>
2023-02-08 19:44:23 +0200

    Added game run method

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import argparse
+from Game import Game
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-d', '--difficulty', default=1, help='set difficulty level; from 1 to 3')
 
 args = parser.parse_args()
+
+Game(args).run()
